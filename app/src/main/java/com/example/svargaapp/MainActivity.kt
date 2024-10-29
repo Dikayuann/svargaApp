859d16583a488086eec077cefb561856cf70f542
Dikayuann <143783797+Dikayuann@users.noreply.github.com>
2024-10-29 20:26:43 +0700

    add activity register and upadate register

--- app/src/main/java/com/example/svargaapp/MainActivity.kt
+++ app/src/main/java/com/example/svargaapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.svargaapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //ekplisit intent untuk memanggil login activity
+        val btnMulai:Button = findViewById(R.id.button)
+        //memanggil activity login
+        btnMulai.setOnClickListener{
+            val intent = Intent (this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
