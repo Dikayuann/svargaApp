e647193312a735d4ce12831f930470749ed42e72
Dikayuann <143783797+Dikayuann@users.noreply.github.com>
2024-12-14 19:30:35 +0700

    menambahkan cart fragment

--- app/src/main/java/com/example/svargaapp/MenuAdapter.kt
+++ app/src/main/java/com/example/svargaapp/MenuAdapter.kt
@@ -3,11 +3,15 @@ package com.example.svargaapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MenuAdapter(private val menuList: List<MenuItem>) : RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
+class MenuAdapter(
+    private var menuList: MutableList<MenuItem>,
+    private val onAddToCart: (MenuItem) -> Unit // Callback untuk aksi tambah ke keranjang
+) : RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_menu, parent, false)
@@ -20,14 +24,27 @@ class MenuAdapter(private val menuList: List<MenuItem>) : RecyclerView.Adapter<M
         holder.description.text = menuItem.description
         holder.price.text = menuItem.price
         holder.image.setImageResource(menuItem.image)
+
+        // Aksi tombol tambah ke keranjang
+        holder.addToCartButton.setOnClickListener {
+            onAddToCart(menuItem)
+        }
     }
 
     override fun getItemCount(): Int = menuList.size
+
+    fun updateMenu(newMenuList: List<MenuItem>) {
+        menuList.clear()
+        menuList.addAll(newMenuList)
+        notifyDataSetChanged()
+    }
 
     inner class MenuViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.textName)
         val description: TextView = itemView.findViewById(R.id.textDescription)
         val price: TextView = itemView.findViewById(R.id.textPrice)
         val image: ImageView = itemView.findViewById(R.id.imageMenu)
+        val addToCartButton: TextView = itemView.findViewById(R.id.btnAdd) // Pastikan ID-nya benar
     }
 }
+
--- app/src/main/java/com/example/svargaapp/MenuFragment.kt
+++ app/src/main/java/com/example/svargaapp/MenuFragment.kt
@@ -4,20 +4,24 @@ import SliderAdapter
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.example.svargaapp.CartRepository.CartRepository
 
 class MenuFragment : Fragment() {
 
     private lateinit var sliderHandler: Handler
     private lateinit var sliderRunnable: Runnable
+    private val buttonList = mutableListOf<TextView>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,13 +41,12 @@ class MenuFragment : Fragment() {
             R.drawable.promo_svarga3,
             R.drawable.promo_svarga4
         )
-
         val sliderAdapter = SliderAdapter(images)
         val viewPager = view.findViewById<ViewPager2>(R.id.viewPager)
         viewPager.adapter = sliderAdapter
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        setCurrentIndicator(0)
 
+        // Timer untuk slider
         sliderHandler = Handler(Looper.getMainLooper())
         sliderRunnable = Runnable {
             if (viewPager.currentItem == images.size - 1) {
@@ -53,32 +56,35 @@ class MenuFragment : Fragment() {
             }
         }
 
-        // Setup RecyclerView for menu items
+        // Daftar item menu
         val menuList = listOf(
-            MenuItem("Cappuccino", "With Milkshake", "Rp. 23.000", R.drawable.janu_profile),
-            MenuItem("Midnight Berry", "Americano rasa buah blueberry", "Rp. 28.000", R.drawable.janu_profile),
-            MenuItem("Coffee Java", "Kopi susu dengan gula aren", "Rp. 25.000", R.drawable.janu_profile),
-            MenuItem("Vietnamese", "Kopi susu dengan krimer kental manis", "Rp. 25.000", R.drawable.janu_profile),
-            MenuItem("Cappuccino", "With Milkshake", "Rp. 23.000", R.drawable.janu_profile),
-            MenuItem("Midnight Berry", "Americano rasa buah blueberry", "Rp. 28.000", R.drawable.janu_profile),
-            MenuItem("Coffee Java", "Kopi susu dengan gula aren", "Rp. 25.000", R.drawable.janu_profile),
-            MenuItem("Vietnamese", "Kopi susu dengan krimer kental manis", "Rp. 25.000", R.drawable.janu_profile)
+            MenuItem("Cappuccino", "With Milkshake", "23000", "Hot", R.drawable.menu1),
+            MenuItem("Midnight Berry", "Americano rasa buah blueberry", "28000", "Cold", R.drawable.menu2),
+            MenuItem("Coffee Java", "Kopi susu dengan gula aren", "25000", "Hot", R.drawable.menu3),
+            MenuItem("Vietnamese", "Kopi susu dengan krimer kental manis", "25000", "Cold", R.drawable.menu4)
         )
 
+        // RecyclerView dan MenuAdapter
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = MenuAdapter(menuList)
+        val menuAdapter = MenuAdapter(menuList.toMutableList()) { selectedItem ->
+            // Tambahkan produk ke keranjang
+            val product = Product(
+                name = selectedItem.name,
+                price = selectedItem.price.toDouble(),
+                quantity = 1,
+                imageResourceId = selectedItem.image
+            )
+            CartRepository.addToCart(product)
+            Toast.makeText(context, "${product.name} added to cart", Toast.LENGTH_SHORT).show()
+            Log.d("MenuFragment", "Product added to cart: $product")
+        }
+        recyclerView.adapter = menuAdapter
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
 
-        // Mengatur LayoutManager menjadi GridLayoutManager dengan 2 kolom
-        val layoutManager = GridLayoutManager(context, 2) // 2 kolom
-        recyclerView.layoutManager = layoutManager
-
-
-        // ViewPager2 callback and slider logic
+        // ViewPager2 callback
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                setCurrentIndicator(position)
                 sliderHandler.removeCallbacks(sliderRunnable)
                 sliderHandler.postDelayed(sliderRunnable, 5000)
             }
@@ -93,18 +99,5 @@ class MenuFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         sliderHandler.postDelayed(sliderRunnable, 5000)
-    }
-
-    private fun setCurrentIndicator(index: Int) {
-        val indicatorLayout = requireView().findViewById<LinearLayout>(R.id.indicatorLayout)
-        val childCount = indicatorLayout.childCount
-        for (i in 0 until childCount) {
-            val indicator = indicatorLayout.getChildAt(i)
-            if (i == index) {
-                indicator.setBackgroundResource(R.drawable.indicator_active)
-            } else {
-                indicator.setBackgroundResource(R.drawable.indicator_inactive)
-            }
-        }
     }
 }
